a83c3987a94be3080514d31b744aec7bb0941ef0
Arpit_jaiswal <jaiswalarpit1414@gmail.com>
2023-11-05 15:00:34 +0530

    create and run server

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,19 @@
 const express=require("express")
+// const mongoose=require("mongoose");
+// const dotenv=require('dotenv');
+
+// dotenv.config();
+
+// mongoose.connect(process.env.MONGO).then(()=>{
+//     console.log("connection successful") 
+// }).catch(()=>{
+//     console.log(process.env.MONGO)
+//     console.log("Error while connection")
+// })
+
  
 const app=express();
 
 app.listen(3000,()=>{
-    console.log("Server is running !! ")
+    console.log("Server is running at 3000 ")
 })
