cc81430663e243d3fa1f68ea1b478c29e1d9bebe
Arpit_jaiswal <jaiswalarpit1414@gmail.com>
2023-11-12 11:26:10 +0530

    some changes done in CreateBook component

--- Frontend/src/pages/CreateBook.jsx
+++ Frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
 import { useState } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase.js';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function CreateBook() {
+
+  // const { currentUser } = useSelector((state) => state.user);
+  // const navigate = useNavigate();
+  const [files,setFiles] = useState([]);
+  console.log(files)
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  console.log(formData);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);  // file is started uploading
+      setImageUploadError(false);
+      const promises = []; 
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,  // old form data + new image 
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false); // no error
+          setUploading(false); // uploaded 
+        })
+        .catch((err) => {
+          setImageUploadError('Image upload failed (2 mb max per image)');
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError('You can only upload 6 images per listing');
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);  // method from firebase
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      // const uploadTask = XMLHttpRequestUpload(storageRef,file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;  // this will show the percentage of image is uploaded 
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+ 
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -52,19 +138,17 @@ export default function CreateBook() {
           <div className='flex gap-6 flex-wrap'>
             <div className='flex gap-2'>
               <input
-                type='radio'
+                type='checkbox'
                 id='sale'
                 className='w-5'
-                name="select"
               />
               <label>Sell</label>
             </div>
             <div className='flex gap-2'>
               <input
-                type='radio'
+                type='checkbox'
                 id='rent'
                 className='w-5'
-                name="select"
               />
               <label>Rent</label>
             </div>
@@ -117,6 +201,7 @@ export default function CreateBook() {
           </p>
           <div className='flex gap-4'>
             <input
+              onChange={(e)=> setFiles(e.target.files)}
               className='p-3 border border-gray-300 rounded w-full'
               type='file'
               id='images'
@@ -125,19 +210,42 @@ export default function CreateBook() {
             />
             <button
               type='button'
+              disabled={uploading}
+              onClick={handleImageSubmit}
               className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
             >
-              upload
+              {uploading ? 'Uploading...' : 'Upload'}
             </button>
           </div>
-          <p className='text-red-700 text-sm'>
-          </p>
-        <button
-            className='p-3 bg-red-600 text-white font-medium rounded-lg uppercase hover:opacity-90 disabled:opacity-80'
+          <p className='text-red-700 text-sm'>{imageUploadError && imageUploadError}</p>
+
+          {formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className='flex justify-between p-3 border items-center'
+              >
+                <img
+                  src={url}
+                  alt='book image'
+                  className='w-20 h-20 object-contain rounded-lg'
+                />
+                <button
+                  type='button'
+                  onClick={() => handleRemoveImage(index)}
+                  className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          <button
+            disabled={loading || uploading}
+            className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
-            Create Book
+            {loading ? 'Adding book...' : 'Add Book'}
           </button>
-          {/* {error && <p className='text-red-700 text-sm'>{error}</p>} */}
+          {error && <p className='text-red-700 text-sm'>{error}</p>}
+
         </div>
       </form>
     </main>
