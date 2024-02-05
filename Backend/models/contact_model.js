bb2a7538e73a1608200a20fc8f01ec75a8350400
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-02-06 00:21:38 +0530

    Conatct admin model changed

--- Backend/models/contact_model.js
+++ Backend/models/contact_model.js
@@ -5,29 +5,34 @@ const contactSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
+      unique:false,
     },
     lastName: {
       type: String,
       required: true,
+      unique:false,
     },
     email: {
       type: String,
       required: true,
+      unique:true,
     },
     phoneNumber: {
       type: String,
       maxLength: 10,
+      unique:true,
     },
     message: {
       type: String,
       required: true, 
+      unique:false,
       
     },
   },
   { timestamps: true }
 );
 
-// contactSchema.index({ firstName: 1, lastName: 1, message: 1 }, { unique: false });
+// contactSchema.createIndex({ firstName: 1, lastName: 1, message: 1 }, { unique: false });
 
 // contactSchema.set('autoIndex', false);
 
