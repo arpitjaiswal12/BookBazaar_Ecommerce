6338891f90b1c98ea004eb16022e951254a6991c
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-05-03 21:58:23 +0530

    Changed index.js

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,21 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import dbConnect from "../config/database.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import authRoute from "../routes/auth_route.js";
 import userRoute from "../routes/user_route.js";
 import createBookRoute from "../routes/createBook_route.js";
 // import contactAdmin from "./routes/contact_route.js"
-import cartRoute from "../routes/addToCart_route.js"
+import cartRoute from "../routes/addToCart_route.js";
 import path from "path";
-
 
 dotenv.config();
 dbConnect();
 
-const __dirname = path.resolve();
-const app=express();
+// const __dirname = path.resolve();
+const app = express();
 
 // middleware to parse json request body
 app.use(express.json());
@@ -24,29 +23,29 @@ app.use(cookieParser());
 
 //mounts (adding/ appending the ) todo API routes :: when user hits the user the bydefault path is atteached with the entered request
 
-app.use("/api/auth", authRoute);  
-app.use("/api/user", userRoute);  
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 app.use("/api/book", createBookRoute);
 app.use("/api/cart", cartRoute);
 // app.use("/api/user",contactAdmin);
 
-app.use(express.static(path.join(__dirname,'/Frontend/dist')));
+// app.use(express.static(path.join(__dirname,'/Frontend/dist')));
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+// app.get('*',(req,res)=>{
+//   res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+// });
+
+app.listen(3000, () => {
+  console.log("Server is running at 3000 ");
 });
-
-app.listen(3000,()=>{
-    console.log("Server is running at 3000 ")
-})
 
 // create a middleware function to handle possible error
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
