7df8bd34d5302a752e558ebdda2f0547f1d62148
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-05-03 22:26:57 +0530

    added vercel backend path

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000' || "https://book-bazaar-ecommerce.vercel.app",
         secure: false,
       },
     },
